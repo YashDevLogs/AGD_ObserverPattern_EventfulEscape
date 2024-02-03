4c994927e4aa23b3890c39d6278266987b712599
Yash <91522919+YashDevLogs@users.noreply.github.com>
2024-02-03 14:55:25 +0530

    Improved UI logic
    Removed the serialized reference of GameViewUI and use the event listener to write custom logic.
    Invoked OnKeyPickedUp in KeyView.

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class KeyView : MonoBehaviour, IInteractable
 {
-    [SerializeField] GameUIView gameUIView;
+
     public void Interact()
     {
         int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
         currentKeys++;
-        gameUIView.UpdateKeyText();
-
         gameObject.SetActive(false);
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(currentKeys);
     }
 }
